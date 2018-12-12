87df7d40d39f5fb207aabcc7bfd0766d84d02e0c
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-12 12:23:08 +0300

    Initilization Add of Project

---
+++ src/main/java/com/example/egemen0petclinic/Egemen0PetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.example.egemen0petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Egemen0PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Egemen0PetClinicApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/egemen0petclinic/Egemen0PetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.egemen0petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Egemen0PetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
