f65a56d66e252a7e17c396ec75ed89d8ee625f20
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-16 17:16:52 +0300

    Adding first POJOs. Closes #2.

---
+++ src/main/java/com/example/egemen0petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.example.egemen0petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/example/egemen0petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.example.egemen0petclinic.model;
+
+public class Vet extends Person {
+}
