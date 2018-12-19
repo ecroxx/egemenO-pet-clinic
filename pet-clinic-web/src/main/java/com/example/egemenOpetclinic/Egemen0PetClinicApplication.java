22d7065afc7d1d3773204a7f6eae27949b9c4c92
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-19 13:35:30 +0300

    Adding Owner Index Page and Controller. Closes #13.

--- pet-clinic-web/src/main/java/com/example/egemenOpetclinic/Egemen0PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/example/egemenOpetclinic/Egemen0PetClinicApplication.java
@@ -2,8 +2,10 @@ package com.example.egemenOpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.egemenOpetclinic"})
 public class Egemen0PetClinicApplication {
 
     public static void main(String[] args) {
