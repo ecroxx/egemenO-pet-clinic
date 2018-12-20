8c49da4d4d7ca2614b71bd7d420eec2af70d8106
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-20 12:22:19 +0300

    Refcatored Map Impl to abstract ID generation Closes #23.

--- pet-clinic-data/src/main/java/com/example/egemenOpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/egemenOpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package com.example.egemenOpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.example.egemenOpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService <T,ID>{
-    protected Map<ID,T> map =new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService <T extends BaseEntity,ID extends Long>{
+    protected Map<Long,T> map =new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -16,8 +15,14 @@ public abstract class AbstractMapService <T,ID>{
         return map.get(id);
     }
 
-    T save(ID id,T object){
-        map.put(id, object);
+    T save(T object){
+        if (object!=null){
+            if((object.getId()) == null){
+                object.setId(generateId());
+            }
+            map.put((object.getId()),object);
+        }else throw new RuntimeException();
+
         return object;
     }
 
@@ -27,5 +32,12 @@ public abstract class AbstractMapService <T,ID>{
 
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    public Long generateId(){
+        Long generatedId;
+       try{ generatedId=Collections.max(map.keySet())+1;}
+       catch (NoSuchElementException e) {generatedId=1L;}
+        return generatedId;
     }
 }
