fb91465a63d34c3b96d295fa96a6c5efa3a256f3
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-25 20:58:07 +0300

    Updated Owners,Pets,Types @Entity classes and also updated BaseEntity and Person classes with @MappedSuperClass(which creates no table). Closes #36.

--- pet-clinic-data/src/main/java/com/example/egemenOpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/egemenOpetclinic/model/Pet.java
@@ -1,12 +1,28 @@
 package com.example.egemenOpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="pets")
 public class Pet extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+
+/*@JoinTable(name = "owner_pets",
+        joinColumns = @JoinColumn(name = "pet_id"),
+        inverseJoinColumns = @JoinColumn(name = "owner_id"))*/
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
