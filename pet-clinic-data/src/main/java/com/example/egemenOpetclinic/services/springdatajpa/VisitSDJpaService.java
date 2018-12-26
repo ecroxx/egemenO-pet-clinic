40943eab05042386c0c41ff9677da6d8abbad88a
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-26 04:22:58 +0300

    Visit SpringDataJPA Service and some loader data of Visit Class to Bootstrap is added .Closes #47.

---
+++ pet-clinic-data/src/main/java/com/example/egemenOpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,50 @@
+package com.example.egemenOpetclinic.services.springdatajpa;
+
+import com.example.egemenOpetclinic.model.Visit;
+import com.example.egemenOpetclinic.repositories.VisitRepository;
+import com.example.egemenOpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpavisit")
+public class VisitSDJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits=new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+
+
+    @Override
+    public void delete(Visit visit) {
+         visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+         visitRepository.deleteById(id);
+    }
+}
--- pet-clinic-web/src/main/java/com/example/egemenOpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/egemenOpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.example.egemenOpetclinic.bootstrap;
 
 import com.example.egemenOpetclinic.model.*;
-import com.example.egemenOpetclinic.services.OwnerService;
-import com.example.egemenOpetclinic.services.PetTypeService;
-import com.example.egemenOpetclinic.services.SpecialityService;
-import com.example.egemenOpetclinic.services.VetService;
+import com.example.egemenOpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -21,11 +18,14 @@ public class DataLoader implements CommandLineRunner {
 
     private final SpecialityService specialtyService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
+    private final VisitService visitService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -94,6 +94,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit=new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setLocalDate(LocalDate.now());
+        catVisit.setDescription("cat parazite");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
