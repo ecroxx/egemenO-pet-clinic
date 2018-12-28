5e4fc93c085f2131bb3010382bbebb7ebcf98cb6
egemenozkan <egemen.ozkan@yahoo.com>
2018-12-29 01:20:53 +0300

    JUnit 4 is upgraded to Junit 5.(Maven Pom files(child(data/web) and parent) are updated ) Closes #4.

--- pet-clinic-web/src/test/java/com/example/egemenOpetclinic/Egemen0PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/example/egemenOpetclinic/Egemen0PetClinicApplicationTests.java
@@ -1,11 +1,17 @@
 package com.example.egemenOpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+//import org.junit.Test;      --  JUNIT 4 ANNOTATION
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//import org.junit.runner.RunWith;  --  JUNIT 4 ANNOTATION
+//import org.springframework.test.context.junit4.SpringRunner;  --  JUNIT 4 ANNOTATION
+
+@ExtendWith(SpringExtension.class)
+//@RunWith(SpringRunner.class)   --  JUNIT 4 ANNOTATION
 @SpringBootTest
 public class Egemen0PetClinicApplicationTests {
 
