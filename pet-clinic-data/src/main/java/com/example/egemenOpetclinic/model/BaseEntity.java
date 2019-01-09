83d45514fdbd8b0a473cff188ffe65db7059ad59
egemenozkan <egemen.ozkan@yahoo.com>
2019-01-09 10:46:42 +0300

    Create/Update Owner page and Controller/Tests are added. (BaseEntity isNew() method is added for createOrUpdate.html form ?'':'' selection )

--- pet-clinic-data/src/main/java/com/example/egemenOpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/egemenOpetclinic/model/BaseEntity.java
@@ -20,6 +20,10 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public boolean isNew(){
+        return this.id==null;
+    }
 /*
     public Long getId() {
         return id;
